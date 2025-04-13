5cf404b2aa538cd6165ddd951ea2c899eb864d37
Shubham Yadav <9026262332sky@gmail.com>
2025-04-13 13:51:33 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post(`${apiUrl}ai/get-review`, { code })
+    const response = await axios.post(`${apiUrl}/ai/get-review`, { code })
     setReview(response.data)
   }
 
